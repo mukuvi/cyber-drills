b0e4ff42f384380b495b372645dee96c4b32801a
mukuvi <jamesmngandu@gmail.com>
2025-11-20 00:23:29 +0300

    commit

---
+++ ptest.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+# print("hello kenya")
+quote ="they will remember me not because of bad deeds or good deeds but because of the life i lived"
+
+# print(quote.title())
+# print(quote.lower())
+# print(quote.upper())
+
+age =22
+name = "james"
+
+# print(name + " is " + str(age) + " old")
+
+def logs():
+    file = "root"
+    system = "Parrot OS"
+   ##  print("I am using " +file+ " file in the "+system)
+logs()
+
+def add_num(num):
+    print(num *num)
+add_num(8)
