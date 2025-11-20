f27a5449f288f53325ded6471b0abc2b65cbbc26
mukuvi <jamesmngandu@gmail.com>
2025-11-20 14:12:43 +0300

    commit

--- scanner.py
+++ scanner.py
@@ -17,4 +17,27 @@ print("Scanning target "+target)
 print("Time started: "+str(datetime.now()))
 print("-" * 50)
 
+try:
+    for port in range(50,85):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1) #is a float
+        result = s.connect_ex((target,port)) #returns error indicator
+        print("Checking port {}".format(port))
+        if result == 0:
+            print("Port {} is open".format(port))
+        s.close()
 
+except KeyboardInterrupt:
+    print("\nExiting program.")
+    sys.exit()
+    
+except socket.gaierror:
+    print("Hostname could not be resolved.")
+    sys.exit()
+
+except socket.error:
+    print("Couldn't connect to server.")
+    sys.exit()
+    
+    
+        
