937a45fef9569bea5b84f09ac7a31b3fa2a47bdb
mukuvi <jamesmngandu@gmail.com>
2025-11-20 10:22:04 +0300

    commit

--- 102.py
+++ 102.py
@@ -1,2 +1,15 @@
-#!/bin/python
-print("heyy")
+#!/bin/bash
+## importing
+import sys # importing system function and parameters
+
+from datetime import datetime
+#print(datetime.now())
+from datetime import datetime as dt #importing with alias
+# print(dt.now())
+
+## Advanced strings
+my_name = "James Ngandu"
+#print(my_name[0:5])
+sentence = " just keep pushing boyy"
+
+print(sentence.split())
